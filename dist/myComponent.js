15c4be36bdeca94db111eca6a113de723f0da0a4
developer <pooniaharkesh196@gmail.com>
2023-05-05 12:35:53 +0530

    initial commit

---
+++ dist/myComponent.js
@@ -0,0 +1,7 @@
+import React from "react";
+const myComponent = props => {
+  return /*#__PURE__*/React.createElement("div", {
+    className: "styled-component"
+  }, props.children);
+};
+export default myComponent;
---
+++ src/components/myComponent.js
@@ -0,0 +1,5 @@
+import React from "react";
+const myComponent = (props) => {
+  return <div className="styled-component">{props.children}</div>;
+};
+export default myComponent;
